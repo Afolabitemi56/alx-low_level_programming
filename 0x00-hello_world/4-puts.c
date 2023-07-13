0f9b0cdaabca6f4e876d86f23dc0b693b7d52b98
Afolabitemi56 <Afoloabitemi123@gmail.com>
2023-07-13 07:43:40 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,5 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle, followed by a new line\n");
-	return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);}
