d71e18151045c4cf1b926ad0499890aa0305f167
Afolabitemi56 <Afoloabitemi123@gmail.com>
2023-07-17 12:10:52 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 48; i < 58; i++)
-        {
+	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
