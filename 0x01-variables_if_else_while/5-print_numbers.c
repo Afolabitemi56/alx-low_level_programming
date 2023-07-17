ddefb7f74e7a9e59ba63b5243b191fdc742bcd00
Afolabitemi56 <Afoloabitemi123@gmail.com>
2023-07-17 07:24:08 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
         for (i = 0; i < 10; i++)
         {
-                printf("%d", i);
+		printf("%d", i);
 	}
 	putchar('\n');
 	return (0);
